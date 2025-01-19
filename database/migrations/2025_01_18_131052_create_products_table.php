841f4f313a2b0c685704742d267ffe8c830a40a8
hamzah <hamzah.dev.061223@gmail.com>
2025-01-19 21:20:46 +0800

    copy code ke linux

--- database/migrations/2025_01_18_131052_create_products_table.php
+++ database/migrations/2025_01_18_131052_create_products_table.php
@@ -14,14 +14,16 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained();
+            $table->string('product_code')->unique();
+            $table->string('qr_code')->nullable();
             $table->string('name');
             $table->string('brand');
             $table->string('specifications');
-            $table->integer('stock');
+            $table->integer('stock')->default(0);
             $table->integer('cost');
             $table->integer('selling_price');
-            $table->string('gambar');
-            $table->string('description');
+            $table->string('gambar')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
