7d99aa5615087fa24030afee4f65b37160d6a958
Hamzah <hamzah.dev.061223@gmail.com>
2025-02-20 12:31:07 +0700

    update logo

--- resources/views/components/layouts/components/sidebar.blade.php
+++ resources/views/components/layouts/components/sidebar.blade.php
@@ -4,7 +4,7 @@
         <!-- Logo Header -->
         <div class="logo-header" data-background-color="dark">
             <a href="index.html" class="logo">
-                <img src="{{ asset('assets/img/logo-biner.png') }}" alt="navbar brand" class="navbar-brand"
+                <img src="{{ asset('assets/img/binstok.png') }}" alt="navbar brand" class="navbar-brand"
                     height="20" />
             </a>
             <div class="nav-toggle">
