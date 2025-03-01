a8ac5128e43c44beef61a2229e0a4461c17c678c
hamzah <hamzah.dev.061223@gmail.com>
2025-03-01 18:24:05 +0800

    update tampilan dashboard

--- resources/views/livewire/admin/admin-dashboard.blade.php
+++ resources/views/livewire/admin/admin-dashboard.blade.php
@@ -84,8 +84,9 @@
             </div>
         </div>
 
+        <h4>Transaksi Harian</h4>
         <div class="row">
-            <div class="col-sm-6 col-md-6">
+            <div class="col-sm-6 col-md-4">
                 <div class="card card-stats card-round">
                     <div class="card-body">
                         <div class="row align-items-center">
@@ -104,12 +105,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-6">
+            <div class="col-sm-6 col-md-4">
                 <div class="card card-stats card-round">
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-icon">
-                                <div class="icon-big text-center icon-success bubble-shadow-small">
+                                <div class="icon-big text-center icon-warning bubble-shadow-small">
                                     <i class="fas fa-luggage-cart"></i>
                                 </div>
                             </div>
@@ -117,6 +118,25 @@
                                 <div class="numbers">
                                     <p class="card-category">Penjualan Harian</p>
                                     <h4 class="card-title">{{ 'Rp. ' . number_format($sales) }}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="card card-stats card-round">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-icon">
+                                <div class="icon-big text-center icon-success bubble-shadow-small">
+                                    <i class="far fa-money-bill-alt"></i>
+                                </div>
+                            </div>
+                            <div class="col col-stats ms-3 ms-sm-0">
+                                <div class="numbers">
+                                    <p class="card-category">Keuntungan</p>
+                                    <h4 class="card-title">{{ 'Rp. ' . number_format($sales - $purchases) }}</h4>
                                 </div>
                             </div>
                         </div>
